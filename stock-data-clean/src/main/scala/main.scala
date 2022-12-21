11d0d972437ba399ccf30f42383a105f2d412613
Jacob Leon <jleon@tubi.tv>
2022-12-20 20:31:14 -0600

    Scala code for data clean initial version

--- stock-data-clean/src/main/scala/main.scala
+++ stock-data-clean/src/main/scala/main.scala
@@ -1,9 +1,13 @@
-import java.sql.DriverManager;
+package com.jacobleon.stocks
+
+import java.sql.DriverManager
 import scala.util.{Try, Success, Failure}
+import java.time.{Instant, ZoneOffset, LocalDateTime, LocalDate}
 
-object cleanData {
+case class StockQuote(ticker:String, close:Float, open:Float, high:Float, low:Float, txn_timestamp:Int, txn_date:Option[LocalDate]=None, volume:Int)
+object CleanData {
 
-    def main(args: Array[String]) = { 
+    def main(args: Array[String]): Unit = { 
         val dm= DriverManager.getConnection("jdbc:sqlite:../stock-quotes.db")
         var statement = dm.createStatement()
         val res = Try(statement.executeQuery("select * from stock_quotes_landing"))
@@ -17,18 +21,21 @@ object cleanData {
                                             open = rs.getFloat("open"),
                                             high = rs.getFloat("high"),
                                             low = rs.getFloat("low"),
-                                            timestamp = rs.getInt("timestamp"),
+                                            txn_timestamp = rs.getInt("timestamp"),
                                             volume = rs.getInt("volume")
                                             )
                 }.toStream
             }
-           // case Failure(ex) => Iterator[StockQuote]
+            case Failure(ex) => {
+                                 println{"Query Failed"}
+                                 throw ex
+                                }
         }
-        val c =p.map(x=> x.copy(ticker= x.ticker.toUpperCase()))
-        //add timestamp to groupby after formatting it for date
-        val o = c.groupBy(_.ticker).values.map(_.maxBy(_.timestamp))
-        c.groupMapReduce(_.ticker)(_.timestamp.toInt)(_ max _)
+        val cleaned = p.map(x=> x.copy(ticker= x.ticker.toUpperCase(), txn_date= Some(LocalDateTime.ofInstant(Instant.ofEpochSecond(x.txn_timestamp), ZoneOffset.UTC).toLocalDate())))
+        val latestRec = cleaned.groupBy(r => (r.txn_date, r.ticker)).values.map(_.maxBy(_.txn_date)) // Retrieve latest record per ticker per day
+        val insertData = (sq :StockQuote) => statement.executeUpdate(s"INSERT INTO stock_quotes VALUES('${sq.ticker}', ${sq.close}, ${sq.open}, ${sq.high}, ${sq.low}, ${sq.txn_timestamp}, '${sq.txn_date.getOrElse("").toString()}', ${sq.volume})")
+        // TODO work on upsert
+        statement.executeUpdate("delete from stock_quotes")
+        latestRec.foreach(insertData)
     }
 }
-
-case class StockQuote(ticker:String, close:Float, open:Float, high:Float, low:Float, timestamp:Int, volume:Int)
